e363b50250c3b84afb1bea1183163f19af458cd0
Jag Talon <jag@jagtalon.com>
2020-06-17 10:43:01 -0400

    Break up server.js into smaller functions

--- server.js
+++ server.js
@@ -2,40 +2,41 @@ const http = require('http');
 const crawler = require("open-graph-scraper");
 
 http.createServer((request, response) => {
-  const { headers, method, url } = request;
+  const { url } = request;
   let body = [];
-  request.on('error', (err) => {
-    errorReporting(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    response.on('error', errorReporting);
 
-    // Make sure to return a JSON response.
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('Access-Control-Allow-Origin', '*');
+  request.on('error', errorReporting)
+    .on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      response.on('error', errorReporting);
 
-    // Remove the slash from the URL.
-    let cleanUrl = url.replace(/^\//, '');
-    
-    // Set the options for our crawler.
-    const options = {
-      'user-agent': 'Figma Open Graph Plugin Bot 1.0',
-      url: cleanUrl
-    };
-    
-    // Crawl the page and retrieve the Open Graph data.
-    crawler(options)
-      .then((data) => {
-          const { result } = data;
-          response.end(JSON.stringify(result));
-      }).catch((error) => {
-          response.end(JSON.stringify(error));
-      });
-  });
+      // Make sure to return a JSON response.
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'application/json');
+      response.setHeader('Access-Control-Allow-Origin', '*');
+  
+      // Crawl the page and retrieve the Open Graph data.
+      fetchOpenGraph(response, url.replace(/^\//, ''));
+    });
 }).listen(8080);
+
+// Crawl the page and retrieve the Open Graph data.
+function fetchOpenGraph(response, url) {
+  const options = {
+    'user-agent': 'Figma Open Graph Plugin Bot 1.0',
+    url: url
+  };
+
+  crawler(options)
+    .then((data) => {
+        const { result } = data;
+        response.end(JSON.stringify(result));
+    }).catch((error) => {
+        response.end(JSON.stringify(error));
+    });
+}
 
 function errorReporting(err) {
   console.error(err);
