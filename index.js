45bedda26ef75570735791d183c9986ffa3e1aba
Jag Talon <jag@jagtalon.com>
2020-08-16 18:02:43 -0400

    Use node-html-parser to parse the HTML

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
+var HTMLParser = require('node-html-parser');
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  const responseHeaders = {
+  // Headers used to respond to the Figma plugin.
+  const responseInit = {
     headers: { 
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
      },
   }
 
-  const requestHeaders = {
-    'User-Agent': 'Figma Open Graph Plugin 1.0'
+  // Headers used to scrape a website.
+  const requestInit = {
+    headers: {
+      'User-Agent': 'Figma Open Graph Plugin 1.0',
+      'Accept': 'text/html;charset=UTF-8'
+    }
   }
 
-  const scrapeResponse = await fetch('http://example.com/', {})
+  const scrapeResponse = await fetch('http://example.com/', requestInit)
   const scrapeResult = await gatherResponse(scrapeResponse)
 
-  return new Response(JSON.stringify({result: scrapeResult, response: scrapeResponse}), responseHeaders)
+  var root = HTMLParser.parse(scrapeResult);
+
+  return new Response(JSON.stringify({result: root}), responseInit)
 }
 
 // This is from Cloudflare's example:
@@ -28,6 +34,7 @@ async function handleRequest(request) {
 async function gatherResponse(response) {
   const { headers } = response
   const contentType = headers.get('content-type') || ''
+
   if (contentType.includes('application/json')) {
     return JSON.stringify(await response.json())
   } else if (contentType.includes('application/text')) {
