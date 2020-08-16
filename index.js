7c03727ae01b6576447ec67645a8042d2053cf74
Jag Talon <jag@jagtalon.com>
2020-08-15 23:10:33 -0400

    Start retrieving HTML from a website

--- index.js
+++ index.js
@@ -6,16 +6,35 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  let response
-
-  if (request.method === 'GET') {
-    response = new Response(JSON.stringify({response: 'Hello worker!'}), {
-      headers: { 
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-       },
-    })
+  const responseHeaders = {
+    headers: { 
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+     },
   }
 
-  return response
+  const requestHeaders = {
+    'User-Agent': 'Figma Open Graph Plugin 1.0'
+  }
+
+  const scrapeResponse = await fetch('http://example.com/', {})
+  const scrapeResult = await gatherResponse(scrapeResponse)
+
+  return new Response(JSON.stringify({result: scrapeResult, response: scrapeResponse}), responseHeaders)
+}
+
+// This is from Cloudflare's example:
+// https://developers.cloudflare.com/workers/templates/pages/fetch_html/
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get('content-type') || ''
+  if (contentType.includes('application/json')) {
+    return JSON.stringify(await response.json())
+  } else if (contentType.includes('application/text')) {
+    return await response.text()
+  } else if (contentType.includes('text/html')) {
+    return await response.text()
+  } else {
+    return await response.text()
+  }
 }
